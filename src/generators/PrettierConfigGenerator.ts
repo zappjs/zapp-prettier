295d1576e9ed54baf06a6b3ef51ea4981df86bdb
Chris Tate <chris@zappjs.com>
2019-07-03 11:56:29 -0500

    v0.1.4

--- src/generators/PrettierConfigGenerator.ts
+++ src/generators/PrettierConfigGenerator.ts
@@ -1,10 +1,10 @@
-import { Generator, generate } from '@zappjs/core';
+import { IGenerator, generate } from '@zappjs/core';
 import { JsonEngine } from '@zappjs/json';
 
 import { IPrettierConfigSpec } from '../interfaces';
 import { PrettierConfigSchema } from '../schemas';
 
-export const PrettierConfigGenerator: Generator = (spec: IPrettierConfigSpec) => generate({
+export const PrettierConfigGenerator: IGenerator<IPrettierConfigSpec> = (spec) => generate({
   engine: JsonEngine,
   schema: PrettierConfigSchema,
   spec
